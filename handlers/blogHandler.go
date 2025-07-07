3198da0bedf53076d0b516d060f3314245a350e7
rogle <raitis.ogle@gmail.com>
2025-07-07 16:34:10 +0300

    update3-07.07.2025

--- handlers/blogHandler.go
+++ handlers/blogHandler.go
@@ -2,9 +2,30 @@ package handlers
 
 import (
 	"database/sql"
+	"html/template"
 	"net/http"
+	"yourproject/models" // adjust to your actual import path
+	"yourproject/sessions"
 )
 
 func BlogHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	tmpl := template.Must(template.ParseFiles("views/blog.html"))
 
+	loggedIn := false
+	cookie, err := r.Cookie("session_token")
+	if err == nil {
+		_, exists := sessions.Sessions[cookie.Value]
+		if exists {
+			loggedIn = true
+		}
+	}
+
+	data := models.PageData{
+		LoggedIn: loggedIn,
+	}
+
+	err = tmpl.ExecuteTemplate(w, "blog.html", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
